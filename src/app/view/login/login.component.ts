578e693faac5fd9610b96b66ca06a2cb756f694c
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-03-09 23:36:24 -0300

    feat: criando formulario reativo

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formulario! : FormGroup;
+
+  submetido = false
+  entrar = true
+  inscrever = false
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nome: ['', [Validators.required]],
+      email: ['',[ Validators.required,  Validators.email]],
+      senha: ['', [Validators.required, Validators.pattern('^(?=.*?[!@#$%¨&*])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')]]
+    })
   }
 
+  btnInscrever(){
+    this.inscrever = true
+    this.entrar = false
+  }
+
+  btnEntrar(){
+    this.entrar = true
+    this.inscrever = false
+  }
+
+  cadastrar():void {
+    console.log(this.formulario.controls)
+    this.submetido = true
+    if (this.formulario.valid){
+      alert("cadastro feito com sucesso")
+    }
+  }
 }
