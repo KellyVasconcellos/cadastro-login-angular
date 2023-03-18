285096789a1f1ce8c4db1c1f7f0c2e5791fe4cb1
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-03-18 12:11:49 -0300

    feat: arrumando modal

--- src/app/view/area-logada/area-logada.component.ts
+++ src/app/view/area-logada/area-logada.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject, timeout } from 'rxjs';
 import { ModalDeletarComponent } from 'src/app/components/modal-deletar/modal-deletar.component';
 import { IUsuario } from 'src/app/interface/usuario';
 import { LoginService } from 'src/app/service/login.service';
@@ -12,28 +12,46 @@ import { LoginService } from 'src/app/service/login.service';
 })
 export class AreaLogadaComponent implements OnInit {
 
-  usuarios! : Array<IUsuario>
+  usuarios : Array<IUsuario> = []
+
+  listasUsuarios = new Subject<Array<IUsuario>>();
+
+  alert = false
 
   constructor(
-    private router: Router,
     private service: LoginService,
     private _modalService: NgbModal,
 
   ) { }
 
   ngOnInit(): void {
-    this.service.getUsuarios().subscribe((resposta : Array<IUsuario>) => {
-      this.usuarios = resposta
+    this.getUsuarios()
+
+    this.listasUsuarios.subscribe((respostaSubject : Array<IUsuario>) => {
+      this.usuarios = respostaSubject
     })
   }
 
-  open() {
+  open(id:number) {
     const modal = this._modalService.open(ModalDeletarComponent, { centered: true });
     modal.closed.subscribe(() => {
-      this.service.deletarUsuario(952121708953).subscribe(() => {
-        this.router.navigateByUrl("/area-logada")
+      this.service.deletarUsuario(id).subscribe(() => {
+        this.getUsuarios()
+        this.alert = true
+        setTimeout(() => {
+          this.alert = false
+        }, 3000);
       })
     })
   }
+
+  getUsuarios() {
+    this.service.getUsuarios().subscribe((respostaApi : Array<IUsuario>) => {
+      this.listasUsuarios.next(respostaApi)
+    })
+  }
+
+
+
 
 }
